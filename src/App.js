e3827de95e7261c6d04ae7bab7a66a5e30f46b9e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-12-06 22:21:09 +0530

    Refactor App to fix a11y issues

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <img src={"https://picsum.photos/500/300?random=1"} alt={"random "} />
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <header className="App-header">
+                <h1>Hello World</h1>
+            </header>
+            <main role="main">
+                <p>Put main content here.</p>
+                <img src={"https://picsum.photos/500/300?random=1"} alt={"random "}/>
+            </main>
+            <footer>
+                Footer
+            </footer>
+        </>
+    );
 }
 
 export default App;
